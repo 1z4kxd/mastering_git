059df88a88d52ad21e08ac79933ec3ea192b3d69
izbassar <izbasarbalgambarov@gmail.com>
2026-08-02 10:32:29 +0500

    Deleted last commit and added comment

--- file2.js
+++ file2.js
@@ -2,3 +2,10 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 console.log(getRandomInt(1, 6));
+
+/* here is the new feature!*/
+const greetUser = (name) => {
+  return `Hello, ${name}!`;
+};
+
+console.log(greetUser(userName));
